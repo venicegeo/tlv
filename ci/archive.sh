a09b6a537d07f69cda5113461604c07415c42c1d
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-04-25 17:18:44 -0400

    Taking out the asciidoctor support until it can be added to the pipeline.

--- ci/archive.sh
+++ ci/archive.sh
@@ -17,16 +17,17 @@ source $root/ci/vars.sh
 # get the code
 git clone --depth 1 https://github.com/time-lapse-viewer/tlv.git
 
-# create the docs
-asciidoctor docs/tlv.adoc
+
+# create the application documentation
+#asciidoctor docs/tlv.adoc
+# this will include the docs in the application jar file
+mv ../docs/tlv.html time_lapse/grails-app/conf/
+
 
 # compile the artifact
 pushd $root/tlv/time_lapse
 	# this will include the external config file in the application jar file
 	mv ../config.yml grails-app/conf/
-
-	# this will include the docs in the application jar file
-	mv ../docs/tlv.html grails-app/conf/
 
 	# this needs to be taken out, otherwise it will cause servlet problems when navigating to the homepage
 	sed -i '/apply plugin:"war"/d' build.gradle
