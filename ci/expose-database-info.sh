26fac03aafd289c182413fd607319a47611c41a6
Kevin Naquin <knaquin@Kevins-MacBook-Pro.local>
2016-06-20 07:45:26 -0400

    Fixed the file path.

--- ci/expose-database-info.sh
+++ ci/expose-database-info.sh
@@ -3,4 +3,4 @@
 
 # this will expose the database information via the web in case manually logging into the database is necessary
 code='def postgres() { render System.getenv("VCAP_SERVICES") }'
-sed -i "18i $code" time_lapse/grails-app/controllers/time_lapse/HomeController.groovy
+sed -i "18i $code" $root/tlv/time_lapse/grails-app/controllers/time_lapse/HomeController.groovy
