9ac19f695e743695a7c9e324ea35e4d655b29196
knaquin <knaquin@radiantblue.com>
2016-07-27 16:42:04 -0400

    The javascripts folder was mis-referenced.

--- ci/add-beachfront-support.sh
+++ ci/add-beachfront-support.sh
@@ -12,7 +12,7 @@ tlv_dialog_file="$root/tlv/time_lapse/grails-app/views/_dialogs.gsp"
 cat $beachfront_dialog_file >> $tlv_dialog_file
 
 # add the beachfront javascript file
-beachfront_javascript_file="$root/plugins/beachfront/grails-app/assets/javascript/beachfront.js"
-tlv_javascript_folder="$root/tlv/time_lapse/grails-app/assets/javascript/"
+beachfront_javascript_file="$root/plugins/beachfront/grails-app/assets/javascripts/beachfront.js"
+tlv_javascript_folder="$root/tlv/time_lapse/grails-app/assets/javascripts/"
 mv $beachfront_javascript_file $tlv_javascript_folder
 "//= require beachfront" >> $tlv_javascript_folder/index-bundle.js
--- ci/add-piazza-support.sh
+++ ci/add-piazza-support.sh
@@ -5,5 +5,5 @@
 cat $root/plugins/piazza/grails-app/views/_login-dialog.gsp >> $root/tlv/time_lapse/grails-app/views/_dialogs.gsp
 
 # add the piazza javascript file
-mv $root/plugins/piazza/grails-app/assets/javascript/piazza.js $root/tlv/time_lapse/grails-app/assets/javascript/
-"//= require piazza" >> $root/tlv/time_lapse/grails-app/assets/javascript/index-bundle.js
+mv $root/plugins/piazza/grails-app/assets/javascripts/piazza.js $root/tlv/time_lapse/grails-app/assets/javascripts/
+"//= require piazza" >> $root/tlv/time_lapse/grails-app/assets/javascripts/index-bundle.js
