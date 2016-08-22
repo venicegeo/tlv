8ebccffa4f88400249de7d8675f5d8fae5aeb709
knaquin <knaquin@radiantblue.com>
2016-08-22 11:56:16 -0400

    Adding log-in dialog.

--- ci/add-gbdx-support.sh
+++ ci/add-gbdx-support.sh
@@ -10,6 +10,7 @@ cat $gbdx_layers_menu_file >> $network_specific_layers_menu_file
 gbdx_dialog_file="$root/plugins/gbdx/grails-app/views/_gbdx-dialogs.gsp"
 tlv_dialog_file="$root/tlv/time_lapse/grails-app/views/_dialogs.gsp"
 cat $gbdx_dialog_file >> $tlv_dialog_file
+cat $root/plugins/gbdx/grails-app/views/_login-dialog.gsp >> $tlv_dialog_file
 
 # add the javascript file
 gbdx_javascript_file="$root/plugins/gbdx/grails-app/assets/javascripts/gbdx.js"
